09649e1b8ea7ad1b6661542a8d673e6c2af0e6d3
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-08-01 20:04:18 +0000

    sum total revenue of stores. output average of stores income. output number of stores making at least M

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+
+puts @total_revenue
+
+@avg_revenue = @total_revenue / Store.count
+
+puts @avg_revenue
+
+@big_boys = Store.where(["annual_revenue >= 1000000"]).count
+
+puts @big_boys
