49c7f71edab16a9e5753de78e2ce626365c89451
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-08-01 20:15:37 +0000

    create employees for store1 and store2. create associations b/w Store and Employee models

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Hannah", last_name: "van Adrichem", hourly_rate: 14)
+@store1.employees.create(first_name: "Madeline", last_name: "Collier", hourly_rate: 14)
+@store1.employees.create(first_name: "Zola", last_name: "McAdie", hourly_rate: 14)
+
+@store2.employees.create(first_name: "Halloween", last_name: "Cat", hourly_rate: 12)
+@store2.employees.create(first_name: "Becca", last_name: "Louw", hourly_rate: 14)
+@store2.employees.create(first_name: "Hazen", last_name: "Price", hourly_rate: 15)
